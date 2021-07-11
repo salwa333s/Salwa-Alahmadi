c7ff5492744c169005d8205ee6fd848944378922
salwa333s <salwa333s68@gmail.com>
2021-07-11 20:57:31 +0300

    editing

--- Lesson2/src/packages/lesson2.java
+++ Lesson2/src/packages/lesson2.java
@@ -1,21 +1,33 @@
 package packages;
-import java.util.Scanner; 
-
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;  
+import java.io.FileInputStream;
+import java.util.Scanner;
+import java.io.PrintWriter; 
 public class lesson2 {
  
-	 public static void main(String[] args) { 
+	 public static void main(String[] args) throws FileNotFoundException { 
 		 
-		  Scanner sc = new Scanner(System.in);
-	      System.out.println("Enter your Email: ");
-	      String input= sc.next();
-	      String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
-	     
-	      boolean result = input.matches(regex);
-	      if(result) {
-	         System.out.println("Given email-id is valid");
-	      } else {
-	         System.out.println("Given email-id is not valid");
-	      } 
+		 //write
+		FileOutputStream RD = new FileOutputStream ("myfile.txt"); 
+		PrintWriter PR = new PrintWriter (RD); 
+		 
+		PR.println(" yes java its the best"); 
+		PR.close(); 
+		System.out.println("File myfile.txt");  
+		
+		//Read a file  
+		
+		FileInputStream WT = new FileInputStream ("myfile.txt");  
+		Scanner sc = new Scanner(WT);    
+		
+		while (sc.hasNext()) { 
+			System.out.print(sc.nextLine());
+		}
+		
+		
+		
+		
 	 }
 	
 }
